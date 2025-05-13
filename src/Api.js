890ec76ecdf12be1ec1f559daec162710583ca09
CharaBJ <narutoespro2@gmail.com>
2025-05-12 17:14:53 -0700

    Desarrollo de una API para UNSPLASH

---
+++ src/Api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const searchImage = async(term) => {
+    const url = 'https://api.unsplash.com/search/photos/?client_id=45kxOXjX5YFWzr5PArB9hUUP9gxNBdxvOEgJsAfnRqE'
+
+    const response = await axios.get(url, {
+        params:{
+            query: term
+        }
+    })
+
+    console.log(response)
+    return response.data.results
+}
+
+export default searchImages
